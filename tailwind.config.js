953d71991c1766e460c96b358a62d2193b284523
Luis Garcia Regardiz <gadeeks@gmail.com>
2021-09-15 21:58:32 -0400

    Fix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    purge: {
+        content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+        safelist: [
+            "bg-blue-600",
+            "bg-green-600",
+            "bg-indigo-600",
+            "bg-purple-600",
+            "bg-pink-600",
+            "bg-yellow-500",
+            "bg-indigo-500",
+            "bg-green-500",
+            "bg-gray-500",
+            "bg-yellow-400",
+            "bg-red-600",
+            "bg-indigo-700",
+            "bg-green-700",
+        ],
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
